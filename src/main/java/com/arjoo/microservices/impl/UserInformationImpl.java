686542d6156a44ba196624ec2ec6e5922684b97b
Arjoo <2009arjoo@gmail.com>
2021-07-10 22:58:26 +0530

    Added hystrix commands

--- src/main/java/com/arjoo/microservices/impl/UserInformationImpl.java
+++ src/main/java/com/arjoo/microservices/impl/UserInformationImpl.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 import com.arjoo.microservices.model.User;
 import com.arjoo.microservices.services.UserInfoService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class UserInformationImpl implements UserInfoService {
@@ -15,7 +16,13 @@ public class UserInformationImpl implements UserInfoService {
 	private RestTemplate restTemplate;
 	
 	@Override
-	@HystrixCommand(fallbackMethod = "getFallbackUserInfo")
+	@HystrixCommand(fallbackMethod = "getFallbackUserInfo",
+			commandProperties = {
+					@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value="2000"),
+					@HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value="5"),
+					@HystrixProperty(name="circuitBreaker.errorThresholdPercentage", value="50"),
+					@HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds", value="5000")
+			})
 	public User getUserInformation(String userId) {
 		return restTemplate.getForObject("http://user-info-service/user/"+userId, User.class);
 	}
